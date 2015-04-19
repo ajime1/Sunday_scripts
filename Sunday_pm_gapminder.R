5e23b80440494c3a1f148821108c20bc23e1d669
ajime1 <alyssa.jimenez@UTSouthwestern.edu>
2015-04-19 13:59:59 -0500

    Playing with functions

---
+++ Sunday_pm_gapminder.R
@@ -0,0 +1,29 @@
+# This is a script to explore the gapminder dataset using R
+# Alyssa Jimenez
+# alyssa.jimenez@UTSouthwestern.edu
+# April 19, 2015
+
+# read in data as tab-delimited
+gap.in <-read.table("output/combined_gapMinder.tsv", 
+                    sep = "\t", 
+                    header= TRUE)
+
+for (row.number in 1:10) {
+  for (col.number in 1:5)
+print (gap.in[row.number,col.number])
+}
+
+gap.in[1:10,1:5]
+
+# custom functions
+
+add.me <- function(x,y){
+  x + y
+}
+
+add.me(3,4)
+
+
+
+
+
