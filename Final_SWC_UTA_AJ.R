84d9eac30e414beffe46cf1a45338280cc2bfbdd
ajime1 <alyssa.jimenez@UTSouthwestern.edu>
2015-04-19 15:42:02 -0500

    created new rmd

--- Final_SWC_UTA_AJ.R
+++ Final_SWC_UTA_AJ.R
@@ -2,3 +2,11 @@
 # Alyssa Jimenez
 # alyssa.jimenez@UTSouthwestern.edu
 # April 19, 2015
+
+# Pick three countries you are interested in. Write an Rmd script to load the data file, 
+#select all the data for that country, and make three scatter plots (one for each country) 
+# that have year on the x-axis and GDP on the y axis. Make sure you load all the libraries 
+# you might need (dplyr, etc). Describe any trends you see for each country using markdown text.
+#Commit your changes using git. You can do this from the command line or in RStudio if you like.
+
+
